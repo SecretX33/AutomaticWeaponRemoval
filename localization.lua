e87f84116d83354e4e63848bdbf53640186ef1b4
SecretX33 <4389757+SecretX33@users.noreply.github.com>
2020-12-05 02:01:35 -0300

    Added a help message to /awr ver command

--- localization.lua
+++ localization.lua
@@ -2,10 +2,11 @@
 -- Version 1.0.5
 
 AWR_HELP1 = "This addon will automatically remove your weapons and bow when Lady Deathwhisper controls you."
-AWR_HELP2 = "Try typing \"/awr toggle\" to toggle the addon state ON or OFF, you can also use \"/awr on\" or \"/awr off\"."
-AWR_HELP3 = "If you want to doublecheck if addon is turned on or off and the reason, you can type \"/awr status\" to get an explanation why the addon is in that state."
+AWR_HELP2 = "Try typing \'/awr toggle\' to toggle the addon state ON or OFF, you can also use \'/awr on\' or \'/awr off\'."
+AWR_HELP3 = "You can doublecheck if addon is turned on or off with the reason by typing \'/awr status\'."
+AWR_HELP4 = "Check addon version with '/awr version'."
 
-AWR_REASON_ADDONISOFF = "|cffffe83bstatus:|r addon is |cffff0000off|r because it was set as OFF by the command \"/awr toggle\"."
+AWR_REASON_ADDONISOFF = "|cffffe83bstatus:|r addon is |cffff0000off|r because it was set as OFF by the command \'/awr toggle\'."
 AWR_REASON_NOTINICC = "|cffffe83bstatus:|r addon is |cffff0000off|r because you are not inside ICC right now."
 AWR_REASON_RAIDDIFFICULTY = "|cffffe83bstatus:|r addon is |cffff0000off|r because your ICC is in 10-man normal mode and Lady doesn't cast Dominate Mind in this difficulty."
 AWR_REASON_INSIDEICC = "|cffffe83bstatus:|r addon is |cff00ff00on|r because you are inside ICC."
